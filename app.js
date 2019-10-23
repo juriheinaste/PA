300c02b356a84eecfdd14cd15d56a9dc4818d641
juriheinaste <juri.heinaste@khk.ee>
2019-10-23 16:18:01 +0300

    ndmetüübid

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("JS test");
+// Andmetüübid
+
+// täisarvud
+console.log(10);
+console.log(-4);
+console.log(0);
+
+// reaalarvud - ujuvkomaarvud
+console.log(2.5);
+console.log(-7,23);
+console.log(-2,0);
+
+// sõned - tekst
+console.log('Hello World!');
+
+// tõeväärtused 
+console.log(true);
+console.log(false);
+
+// nimekiri
+console.log([1, 2, 3, 4]);
+
+// objektid
+console.log({a : 1, b : 2});
+console.table({a : 1, b : 2});

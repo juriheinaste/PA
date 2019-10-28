3387de325b92e7b4ee236c995b3efe82eb796c14
juriheinaste <juri.heinaste@khk.ee>
2019-10-28 11:12:25 +0200

    For nimekirja töötlus

--- app.js
+++ app.js
@@ -2,21 +2,8 @@
 
 // fot tsükkel
 // üldtsükkel
-let arv;
-for (arv = 1; arv <=10; arv ++) {
-    lause = 'arv = ';
-    if(arv < 10) {
-        lause = lause + ' ';
-    }
-    lause = lause + arv;
-    if(arv % 2 == 0){
-        lause = lause + ' - paaris';
-    } else {
-        lause = lause + ' - paaritu';
-        //continue; - jätab selle tehte console.log välja
-    }
-    console.log(lause);
-    if(arv >= 5){
-        //break; - viib tehte selle arvuni ja siis lõpetab väljastamise
-    }
+let autod = ['Kia', 'Toyota', 'Ford', 'Nissan'];
+//väljastus
+for(let i = 0; i < autod.length; i++){
+    console.log(autod[i]);
 }

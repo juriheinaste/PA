4abbcb85d1d11f57e864fa1c52feeb1aea2f57d5
juriheinaste <juri.heinaste@khk.ee>
2019-10-28 11:08:12 +0200

    Tsüklite näited

--- app.js
+++ app.js
@@ -1,75 +1,22 @@
-// Muutujad
+// Tsüklid
 
-// var
-// muutujate defineerimine
-var nimi = 'Jüri'; // defineerimine
-console.log(nimi);
-nimi = 'Tom'; // väärtuse omistamine
-console.log(nimi);
-
-// muutujate inistialiseerimine
-var tervitus;
-console.log(tervitus);
-tervitus = 'Tere tulemast!';
-console.log(tervitus);
-
-// liitnimede kasutamine
-var eesNimi = 'Jüri'; //Camel Style
-var ees_nimi = 'Jüri'; //PHP Style
-var EesNimi = 'Jüri'; //Pascal Style
-var eesnimi = 'Jüri';
-
-// let
-let pereNimi = 'Heinaste';
-console.log(pereNimi);
-pereNimi = '?????';
-console.log(pereNimi);
-
-// const
-const minuEesNimi = 'Jüri';
-console.log(minuEesNimi);
-// minuEesNimi = 'Jurr';
-
-// const kasutamise näide
-const inimene = {nimi : 'Katrin', vanus : 20};
-console.table(inimene);
-inimene.vanus = 21;
-console.table(inimene);
-
-// const kasutamise näide 2
-const arvud = [1, 2, 3, 4, 5];
-console.log(arvud);
-arvud.push(6);
-console.log(arvud);
-
-/*let passWord = prompt('Your Password: ');
-if (passWord === '1234') {
-    alert ('Welcome!');
-} else {
-    alert ('Wrong Password. Try Again!');
+// fot tsükkel
+// üldtsükkel
+let arv;
+for (arv = 1; arv <=10; arv ++) {
+    lause = 'arv = ';
+    if(arv < 10) {
+        lause = lause + ' ';
+    }
+    lause = lause + arv;
+    if(arv % 2 == 0){
+        lause = lause + ' - paaris';
+    } else {
+        lause = lause + ' - paaritu';
+        //continue; - jätab selle tehte console.log välja
+    }
+    console.log(lause);
+    if(arv >= 5){
+        //break; - viib tehte selle arvuni ja siis lõpetab väljastamise
+    }
 }
-*/
-
-/*let myAge = prompt('How old are you?');
-if (myAge == 0){
-    alert ('HA HA, nice one!');
-}
-    else if (myAge < 14){
-      alert ('Too young, turn around!');
-
-} else if (myAge == 14){
-    alert ('Wait a little!');
-}
-else {
-    alert ('You may proceed!');
-}
-*/
-
-/*let moneyInBank = parseInt(prompt('How much do you have?'));
-let moneyOut = parseInt(prompt('How much do you want to withdraw?'))
-if (moneyOut > moneyInBank){
-    alert('Not enough money')
-} else {
-    let moneyLeft = moneyInBank - moneyOut
-    alert ('You have ' + moneyLeft + ' on your account')
-}*/

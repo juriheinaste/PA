7864f8bd25f26ddaa6db4f9126f66f3bde500c64
juriheinaste <55825772+juriheinaste@users.noreply.github.com>
2019-11-04 21:34:48 +0200

    Add files via upload

---
+++ arvud.js
@@ -0,0 +1,13 @@
+algarvud= []
+for (var i = 1; i<20;i++){
+    let count = 0;
+    for (var k = 1; k <20;k++){
+        if (i % k == 0){
+            count = count+1;
+        }
+    }
+    if (count == 2){
+        algarvud.push(i);
+    }
+}
+console.log(algarvud)

38c83cb26d02ddc7591bf916abcca303c97217a4
juriheinaste <55825772+juriheinaste@users.noreply.github.com>
2019-11-08 19:57:05 +0200

    Paarsus ja väljastus eraldi funktisoonidega

--- app.js
+++ app.js
@@ -2,16 +2,24 @@
 
 // Funktsioon, mis arvutab 
 // riistküliku pindala
-// ja väljastab ekraani
 
 function riistkylikuPindala(pikkus, laius){
+  // arvutame pindala
+  let pindala = pikkus * laius;
+  // tagastame pindala põhiprogrammi
+  return pindala;
+}
+
+// Funktsioon, mis väljastab tulemus ekraani
+function valjasta(pikkus, laius){
   // väljastame väärtused
   console.log('Pikkus = ' + pikkus);
   console.log('Laius = ' + laius);
-  // arvutame pindala
-  let pindala = pikkus * laius;
-  // väljastame pindala
+  // salvestame arvutatud pindala
+  let pindala = riistkylikuPindala(pikkus, laius);
+  // väljastame tulemus
   console.log('Pindala = ' + pindala);
 }
 
-riistkylikuPindala(15, 5);
+valjasta(15, 5);
+valjasta(1, 5);

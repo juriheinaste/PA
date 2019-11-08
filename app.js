6eb27603748cd238d7a7f8876f39da5e0fd02daf
juriheinaste <55825772+juriheinaste@users.noreply.github.com>
2019-11-08 19:55:55 +0200

    Riistküliku pindala arvutamine koos väljastamisega

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 // Funktsioonid
 
-// arvu paarsuse kontroll
-// funktsiooni defineerimine
-/* 
-function funktsiooniNimi([param1, param2, .., paramN]){
-  funktsiooni sisu
+// Funktsioon, mis arvutab 
+// riistküliku pindala
+// ja väljastab ekraani
+
+function riistkylikuPindala(pikkus, laius){
+  // väljastame väärtused
+  console.log('Pikkus = ' + pikkus);
+  console.log('Laius = ' + laius);
+  // arvutame pindala
+  let pindala = pikkus * laius;
+  // väljastame pindala
+  console.log('Pindala = ' + pindala);
 }
-*/
-function paarsuseKontroll(arv){
-  // tulemuslause defineerimine
-  let lause;
-  // arvutame jääk 2-ga jagamisel
-  let jaak = arv % 2;
-  // kui jaak on võrdne nulliga
-  if( jaak === 0 ){
-    // tegu on paaris avuga
-    lause =  arv + ' on paaris';
-  } else {
-    // tegu on paaritu arvuga
-    lause =  arv + ' on paaritu';
-  }
-  // tagastame antud väärtus
-  return lause;
-}
-// funktsiooni kasutamine
-let paarsus;
-paarsus = paarsuseKontroll(4);
-console.log(paarsus);
+
+riistkylikuPindala(15, 5);

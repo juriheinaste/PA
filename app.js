1fca55419bf91f39489c370f3065aa2733144741
juriheinaste <55825772+juriheinaste@users.noreply.github.com>
2019-11-08 19:58:38 +0200

    Vaikimis väärtuste kasutamine parameetrites

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ function riistkylikuPindala(pikkus, laius){
   return pindala;
 }
 
+
 // Funktsioon, mis väljastab tulemus ekraani
-function valjasta(pikkus, laius){
+function valjasta(pikkus = 0, laius = 0){
   // väljastame väärtused
   console.log('Pikkus = ' + pikkus);
   console.log('Laius = ' + laius);
@@ -23,3 +24,4 @@ function valjasta(pikkus, laius){
 
 valjasta(15, 5);
 valjasta(1, 5);
+valjasta();
